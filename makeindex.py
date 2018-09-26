850cf836b9c1f981469f8a9bde935e899daedcaf
Shahram Rahatlou <rahatlou@dot1x130.roma1.infn.it>
2018-09-26 15:09:06 +0200

    file to create index of all cc cpp and h files for a directory

---
+++ makeindex.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import os, sys
+
+mypath = "."
+
+files = os.listdir(mypath)
+fname = 'test.html'
+
+if len(sys.argv)> 1:
+ fname  = sys.argv[1] + '/' + fname
+ print 'creating ', fname, ' in ', str(sys.argv[1])
+else:
+ print 'creating ', fname, ' in current directory'
+ 
+out = open("test.html","w+")
+
+out.write( '<html>\n' )
+out.write( '  <body>\n' )
+out.write( '    <ul>\n' )
+
+for f in files:
+  if f.endswith(".cpp") or f.endswith(".cc") or f.endswith(".h"):
+    out.write( '       <li><a href=' + f + '>' + f + '</a></li>\n' )
+
+
+out.write( '    </ul>\n' )
+out.write( '  </body>\n' )
+out.write( '</html>\n' )
